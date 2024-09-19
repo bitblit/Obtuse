4c4c044785017c470106040dc2c95ef107c85cd2
Christopher Weiss <bitblit@gmail.com>
2024-09-18 21:47:57 -0700

    Add to index

--- projects/ngx-obtuse-common/src/public-api.ts
+++ projects/ngx-obtuse-common/src/public-api.ts
@@ -5,9 +5,16 @@
 export * from './lib/dialogs/alert/alert.component';
 export * from './lib/dialogs/block-ui/block-ui.component';
 
+export * from './lib/pipes/capitalize.pipe';
 export * from './lib/pipes/dollar-formatted.pipe';
 export * from './lib/pipes/map-values.pipe';
+export * from './lib/pipes/number-with-commas.pipe';
 export * from './lib/pipes/order-by.pipe';
+export * from './lib/pipes/percent-formatted.pipe';
+export * from './lib/pipes/plural.pipe';
+export * from './lib/pipes/round.pipe';
+export * from './lib/pipes/time-ago-formatted.pipe';
+export * from './lib/pipes/timing.pipe';
 
 export * from './lib/services/local-storage.service';
 export * from './lib/services/graphql-query.service';
